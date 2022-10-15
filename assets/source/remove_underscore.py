8462caa740283e21b3950ac9c3a2b73432b39d6c
password <salt.lion@protonmail.com>
2022-10-15 19:26:48 +0300

    align items

--- assets/source/remove_underscore.py
+++ assets/source/remove_underscore.py
@@ -9,7 +9,14 @@ import os
 
 for root, dirs, files in os.walk("../sprites-override"):
    for name in files:
+      # for some files, we don't want prefixes. remove the added _ by inkscape.
+      new_name = name.lstrip("_")
+
+      # for some files, we want prefixes. but separated with "-", not "_"
+      new_name = new_name.replace("_", "-")
+
       old_path = os.path.join(root, name)
-      new_path = os.path.join(root, name.lstrip("_"))
-      print(old_path, new_path)
-      os.rename(old_path, new_path)
+      new_path = os.path.join(root, new_name)
+
+      if new_path != old_path:
+         os.rename(old_path, new_path)
